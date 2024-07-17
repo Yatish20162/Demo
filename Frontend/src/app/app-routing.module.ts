6025f9cff513020b257ce352e3de6ffcc5a182e7
Amit Maindola <amitkm.knp2019@gmail.com>
2024-07-17 10:41:24 +0530

    route configuration

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ManagerDashboardComponent } from './components/manager-dashboard/manager-dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { EmployeeDashboardComponent } from './components/employee-dashboard/employee-dashboard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'manager-dashboard',
+    component: ManagerDashboardComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'employee-dashboard',
+    component: EmployeeDashboardComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin-dashboard',
+    component: AdminDashboardComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
