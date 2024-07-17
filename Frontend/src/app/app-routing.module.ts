febd243ed04ad478964438adf09b7dc520f06307
Simran Vishrant <simran.vishrant@ukg.com>
2024-07-17 16:14:57 +0530

    home dashboard done

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+      path:'',
+      component:HomeComponent,
+      pathMatch:"full"
+    },
+
+    {
+      path: 'login',
+      component: LoginComponent,
+      title: "IJP - Login",
+       pathMatch:"full"}
+
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ManagerDashboardComponent } from './components/manager-dashboard/manage
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { EmployeeDashboardComponent } from './components/employee-dashboard/employee-dashboard.component';
 import { HomeComponent } from './components/home/home.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,14 +16,16 @@ import { HomeComponent } from './components/home/home.component';
     ManagerDashboardComponent,
     AdminDashboardComponent,
     EmployeeDashboardComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
